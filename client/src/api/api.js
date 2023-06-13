63faf2b11a2381a933a4cdc71a18a9fc30349d58
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2023-06-13 08:25:24 +0700

    finish dev

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,27 +1,6 @@
 import axios from "../config";
 
-// //  FETCH API JOB 
-export const fetchApiData = async (url) => {
-  return await axios
-    .get(url)
-    .then((response) => response.data)
-    .catch((response) => response.data);
-};
-
-export const storeApiData = async (url, data) => {
-  return await axios
-    .post(url, data)
-    .then((response) => response.data)
-    .catch((response) => response.response.data);
-};
-
-export const deleteApiData = async (url) => {
-  return await axios
-    .delete(url)
-    .then((response) => response.data)
-    .catch((response) => response.response.data);
-};
-
+z
 // FETCH API USER
 
 
