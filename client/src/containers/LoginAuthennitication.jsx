0c6a4ece5f21eedefbcb59ca1d9694db0ce37bae
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2023-06-13 08:16:13 +0700

    have solve conflict

--- client/src/containers/LoginAuthennitication.jsx
+++ client/src/containers/LoginAuthennitication.jsx
@@ -43,7 +43,7 @@ export const LoginUser = () => {
     const handelSubmit = async (e) => {
           const status= await userLogin(e)
           if (status.data==200) {
-            navigate("/homePage")
+            navigate("/")
           }
         
     }
@@ -147,7 +147,7 @@ export const LoginEmployee = () => {
     const handelSubmit = async (e) => {
         const status= await employeeLogin(e)
         if (status.data==200) {
-            navigate("/homePage")
+            navigate("/")
         }
         
     }
