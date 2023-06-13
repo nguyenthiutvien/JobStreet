9bdcb85b8a39ab1cc8cef8f992e0ab4c47b74abf
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2023-06-13 08:30:09 +0700

    Vien update dev branch because some conflict

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,27 @@
 import axios from "../config";
 
-z
+// //  FETCH API JOB 
+export const fetchApiData = async (url) => {
+  return await axios
+    .get(url)
+    .then((response) => response.data)
+    .catch((response) => response.data);
+};
+
+export const storeApiData = async (url, data) => {
+  return await axios
+    .post(url, data)
+    .then((response) => response.data)
+    .catch((response) => response.response.data);
+};
+
+export const deleteApiData = async (url) => {
+  return await axios
+    .delete(url)
+    .then((response) => response.data)
+    .catch((response) => response.response.data);
+};
+
 // FETCH API USER
 
 
