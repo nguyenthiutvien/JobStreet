a585076cf25a8314224cb3b57db450dc7e6e2788
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2023-06-28 10:37:50 +0700

    update

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,6 @@
-import axios from "../config";
+import React from 'react'
+import axios from 'axios'
+import "../config"
 
 // //  FETCH API JOB 
 export const fetchApiData = async (url) => {
@@ -22,14 +24,11 @@ export const deleteApiData = async (url) => {
     .catch((response) => response.response.data);
 };
 
-// FETCH API USER
-
-
 export const postUser =async (e) => {
   return (
       await axios.post("http://127.0.0.1:8000/api/users",e)
   )
-}
+} 
 export const getUser = async ()=>{
   return (
     await axios.get("http://127.0.0.1:8000/api/users")
@@ -61,9 +60,6 @@ export const userLogin=async(e)=>{
 
 
 
-
-
-
 export const employeeLogin=async(e)=>{
   return(
     axios.post("http://127.0.0.1:8000/api/company/login",e)
@@ -74,6 +70,7 @@ export const postCompany=async (employee)=>{
     await axios.post("http://127.0.0.1:8000/api/company",employee)
   )
 }
+
 export const getCompany=async ()=>{
   return (
     await axios.get("http://127.0.0.1:8000/api/company")
@@ -99,3 +96,106 @@ export const resetPasswordEmployee=async(email,password)=>{
 }
 
 
+export const UserChangePassword=async(id,password)=>{
+  return (
+    await axios.put(`http://127.0.0.1:8000/api/user/change-password/${id}`,password)
+  )
+}
+//Application
+
+export const getTokenUser = async (token)=>{
+  return (
+    await axios.post(`http://127.0.0.1:8000/api/user/get-token/${token}`)
+  )
+}
+export const updateUser=async (id,formData)=>{
+  return (
+    await axios.post(`http://127.0.0.1:8000/api/user/update/${id}`,formData)
+  )
+}
+
+
+export const getApplications=async (email)=>{
+  return (
+      await axios.get(`http://127.0.0.1:8000/api/user/${email}/apply`)
+    )
+}
+export const postApplication=async (formData)=>{
+  return (
+    await axios.post("http://127.0.0.1:8000/api/applications",formData)
+  )
+}
+
+
+// export const getCV=async (name)=>{
+//   return (
+//     await axios.get(`http://127.0.0.1:8000/api/cv/${name}`)
+//   )
+// }
+// Company Management
+
+export const getTokenCompany= async (token)=>{
+  return (
+    await axios.post(`http://127.0.0.1:8000/api/company/get-token/${token}`)
+  )
+}
+export const updateCompanyInfo=async (id,formData)=>{
+  return (
+    await axios.post(`http://127.0.0.1:8000/api/company/update/${id}`,formData)
+  )
+}
+
+ export const getApplicationsCompany = async (token) => {
+    return await axios.get(`http://127.0.0.1:8000/api/get-applications/${token}`);
+};
+
+export const getJobsCompany = async (token) => {
+  return await axios.get(`http://127.0.0.1:8000/api/get-jobs/${token}`);
+};
+
+export const addJob = async (jobData) => {
+ 
+  return  await axios.post(`http://127.0.0.1:8000/api/add-jobs`, jobData)
+    
+
+  
+ 
+};
+export const updateJob = async (token, jobId, jobData) => {
+  try {
+    const response = await axios.put(`http://127.0.0.1:8000/api/update-jobs/${jobId}`, jobData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const deleteJob = async (token, jobId) => {
+  try {
+    const response = await axios.delete(`http://127.0.0.1:8000/api/delete-jobs/${jobId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+export const userComment=async(dataCommet)=>{
+  return (
+    await axios.post("http://127.0.0.1:8000/api/comment",dataCommet)
+  )
+}
+export const getUserComment=async(post_id)=>{
+  return (
+    await axios.get(`http://127.0.0.1:8000/api/comment/${post_id}`)
+  )
+}
