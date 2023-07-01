d7ff032f2a56dc5b9b9cba3049214354c01d6a06
Muas09 <116275467+Muas09@users.noreply.github.com>
2023-07-01 10:55:45 +0700

    update register

--- client/src/containers/Router.jsx
+++ client/src/containers/Router.jsx
@@ -16,19 +16,19 @@ import { Application } from "./Application";
 export const Router = () => {
     return (
         <Routes>
-            <Route path="/a" element={<Application/>}></Route>
-            <Route path="/employeePayment" element={<EmployeePayment/>}></Route>
-            <Route path="/entercodeEmployee" element={<EmployeeEnterCode/>}></Route>
-            <Route path="/entercodeUser" element={<UserEnterCode/>}></Route>
-            <Route path="/newpasswordUser" element={<UserChangePassword/>}></Route>
-            <Route path="/newpasswordEmployee" element={<EmployeeChangePassword/>}></Route>
-            <Route path="/employeeForgot" element={<EmployeeFogotPassword/>}></Route>
-            <Route path="/userForgot" element={<UserFogotPassword/>}></Route>
-            <Route path="/loginEmployee" element={<LoginEmployee/>}></Route>
-            <Route path="/" element={<LoginUser/>}></Route>
-            <Route path="/registerEmployee" element={<RegisterEmployee/>}></Route>
-            <Route path="/registerUser" element={<Register/>}></Route>
-            <Route path="/homePage" element={<HomePage/>}></Route>
+            <Route path="/a" element={<Application />}></Route>
+            <Route path="/employeePayment" element={<EmployeePayment />}></Route>
+            <Route path="/entercodeEmployee" element={<EmployeeEnterCode />}></Route>
+            <Route path="/entercodeUser" element={<UserEnterCode />}></Route>
+            <Route path="/newpasswordUser" element={<UserChangePassword />}></Route>
+            <Route path="/newpasswordEmployee" element={<EmployeeChangePassword />}></Route>
+            <Route path="/employeeForgot" element={<EmployeeFogotPassword />}></Route>
+            <Route path="/userForgot" element={<UserFogotPassword />}></Route>
+            <Route path="/loginEmployee" element={<LoginEmployee />}></Route>
+            <Route path="/" element={<LoginUser />}></Route>
+            <Route path="/registerEmployee" element={<RegisterEmployee />}></Route>
+            <Route path="/registerUser" element={<Register />}></Route>
+            <Route path="/homePage" element={<HomePage />}></Route>
         </Routes>
     );
 };
